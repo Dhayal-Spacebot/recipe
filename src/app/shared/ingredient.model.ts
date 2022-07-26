6b60be0062fcabd79e61c452b3ac84275a28632a
Dhayal-Spacebot <logintodhayal@gmail.com>
2022-07-26 10:40:14 +0530

    ingredient model class

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,14 @@
+export class Ingredient{
+  // public name: string;
+  // public quantity: number;
+
+  // constructor(name:string, amount:number){
+  //   this.name = name;
+  //   this.quantity = amount;
+
+  // } same thing can be done with the shortcut provided in TS
+
+
+  constructor(public name:string, public amount: number){}
+}
+
